e667ec29db620ce033ac3536c602dcc40c6b9b1f
GCast31 <asgardwyn@gmail.com>
2023-01-29 01:05:44 +0100

    main

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,10 @@ struct MyTestApp {
 impl EventLoop for MyTestApp {
     fn load(&mut self, gc2d: &mut Gc2d) -> Result<(), gc2d::event::EventError> {
         gc2d.window.set_title("MyApp");
-
         Ok(())
     }
 
     fn draw(&mut self, gc2d: &mut Gc2d, fonts: &mut gc2d::fonts::FontsManager) -> Result<(), gc2d::event::EventError> {
-
-
         Ok(())
     }
 }
