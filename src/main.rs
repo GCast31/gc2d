d7d5b77fb0cf77b6b8edd7ee30d4f8f8c3367b9f
GCast31 <asgardwyn@gmail.com>
2023-02-14 13:53:24 +0100

    Menage

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,22 @@
 
+/*
+ * For using gc2d
+ * ==============
+ * 
+ * Create a struct  
+ * Implement EventLoop for the struct
+ * 
+ * Gc2d::new().run(the struct);
+ * 
+ * Todo : Audio multi-chanel
+ * Todo : Audio Stop / Pause ...
+ * Todo : FPS to optimize
+ * Todo : Optimize graphics draw
+ * Todo : Add more bindings keys in keyboard
+ * Todo : Add error
+ * Todo : Test Quad / scale ...
+ */
+
 use gc2d::color::Color;
 use gc2d::gc2d::Gc2d;
 use gc2d::event::EventLoop;
@@ -8,22 +26,23 @@ struct MyTestApp {
 }
 
 impl EventLoop for MyTestApp {
-    fn load(&mut self, gc2d: &mut Gc2d, audio_manager: &mut gc2d::audio::AudioManager) -> Result<(), gc2d::event::EventError> {
+    fn load(&mut self, gc2d: &mut Gc2d, _audio_manager: &mut gc2d::audio::AudioManager) -> Result<(), gc2d::event::EventError> {
         //gc2d.audio.new_source("assets/sounds/cool.mp3", audio_manager, gc2d::audio::AudioType::Stream);
         //gc2d.audio.play(audio_manager, "assets/sounds/cool.mp3", -1);
-        
+        gc2d.graphics.set_background_color(Color::WHITE);
         gc2d.graphics.new_font("assets/fonts/PixelMaster.ttf", 25);
         Ok(())
     }
 
-    fn update(&mut self, gc2d: &mut Gc2d, dt: f32, audio_manager: &mut gc2d::audio::AudioManager) -> Result<(), gc2d::event::EventError> {
+    fn update(&mut self, gc2d: &mut Gc2d, _dt: f32, _audio_manager: &mut gc2d::audio::AudioManager) -> Result<(), gc2d::event::EventError> {
         self.x = gc2d.mouse.x;
         
         Ok(())
     }
 
-    fn draw(&mut self, gc2d: &mut Gc2d, fonts: &mut gc2d::fonts::FontsManager, dt: f32) -> Result<(), gc2d::event::EventError> {
+    fn draw(&mut self, gc2d: &mut Gc2d, fonts: &mut gc2d::fonts::FontsManager, _dt: f32) -> Result<(), gc2d::event::EventError> {
         gc2d.graphics.circle(gc2d::graphics::DrawMode::Line, self.x, 50f32, 20f32, Some(Color::BLUE));
+        gc2d.graphics.print(format!("x: {}", gc2d.mouse.x), 10., 10., Some(Color::RED), fonts);
         Ok(())
     }
 
