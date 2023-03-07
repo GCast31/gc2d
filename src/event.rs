0868e63939511a9d569c726ce7342092e7a94717
GCast31 <asgardwyn@gmail.com>
2023-03-07 21:10:06 +0100

    Get width/height + Gc2dResult

--- src/event.rs
+++ src/event.rs
@@ -1,33 +1,27 @@
 
 use sdl2::EventPump;
 
-use crate::{gc2d::Gc2d, context::Context, fonts::FontsManager, keyboard::KeyCode, audio::AudioManager};
-
-
-#[derive(Debug)]
-pub enum EventError {
-
-}
+use crate::{gc2d::{Gc2d, Gc2dResult}, context::Context, fonts::FontsManager, keyboard::KeyCode, audio::AudioManager};
 
 #[allow(unused_variables)]
 pub trait EventLoop {
-    fn load(&mut self, gc2d: &mut Gc2d, audio_manager: &mut AudioManager) -> Result<(), EventError> {
+    fn load(&mut self, gc2d: &mut Gc2d, audio_manager: &mut AudioManager) -> Gc2dResult<()> {
         Ok(())
     }
 
-    fn draw(&mut self, gc2d: &mut Gc2d, fonts: &mut FontsManager, dt: f32) -> Result<(), EventError> {
+    fn draw(&mut self, gc2d: &mut Gc2d, fonts: &mut FontsManager, dt: f32) -> Gc2dResult<()> {
         Ok(())
     }
 
-    fn update(&mut self, gc2d: &mut Gc2d, dt: f32, audio_manager: &mut AudioManager) -> Result<(), EventError> {
+    fn update(&mut self, gc2d: &mut Gc2d, dt: f32, audio_manager: &mut AudioManager) -> Gc2dResult<()> {
         Ok(())
     }
 
-    fn key_pressed(&mut self, gc2d: &mut Gc2d, key: KeyCode) -> Result<(), EventError> {
+    fn key_pressed(&mut self, gc2d: &mut Gc2d, key: KeyCode) -> Gc2dResult<()> {
         Ok(())
     }
 
-    fn key_released(&mut self, gc2d: &mut Gc2d, key: KeyCode) -> Result<(), EventError> {
+    fn key_released(&mut self, gc2d: &mut Gc2d, key: KeyCode) -> Gc2dResult<()> {
         Ok(())
     }
 }
